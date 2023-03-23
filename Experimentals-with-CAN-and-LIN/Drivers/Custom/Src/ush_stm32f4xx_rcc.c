8c9ebade89cd3842d86402ba7fbc47ecba42ec21
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-03-23 13:07:26 +0100

    [_rcc.h/_rcc.c] feat: add RCC module

---
+++ Experimentals-with-CAN-and-LIN/Drivers/Custom/Src/ush_stm32f4xx_rcc.c
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    ush_stm32f4xx_rcc.c
+  * @author  Ulad Shumeika
+  * @version v1.0
+  * @date    21 March 2023
+  * @brief	 This file contains the implementation of functions for working with RCC.
+  *
+  * NOTE: This file is not a full-fledged RCC driver, but contains only some of
+  * 	  the functions that are needed for the current project.
+  ******************************************************************************
+  */
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "ush_stm32f4xx_rcc.h"
