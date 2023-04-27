ecf9fcd771f4c7c4e8e1a6ce18f0d6beb71a0398
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-04-27 14:48:21 +0200

    [bxCAN.c] feat: add bxCAN_CAN1_init function

--- Experimentals-with-CAN-and-LIN/Core/Src/bxCAN.c
+++ Experimentals-with-CAN-and-LIN/Core/Src/bxCAN.c
@@ -4,10 +4,25 @@
 #include "bxCAN.h"
 
 //---------------------------------------------------------------------------
+// Defines
+//---------------------------------------------------------------------------
+#define USE_CAN						(CAN1)
+
+//---------------------------------------------------------------------------
 // Descriptions of FreeRTOS elements
 //---------------------------------------------------------------------------
 static osThreadId sendMessagesHandle;
 static osThreadId receiveMessagesHandle;
+
+//---------------------------------------------------------------------------
+// Structure definitions
+//---------------------------------------------------------------------------
+USH_CAN_settingsTypeDef canInit = {0};
+
+//---------------------------------------------------------------------------
+// Static function prototypes
+//---------------------------------------------------------------------------
+static void bxCAN_CAN1_init(void);
 
 //---------------------------------------------------------------------------
 // FreeRTOS's threads
@@ -20,6 +35,8 @@ static osThreadId receiveMessagesHandle;
   */
 void bxCAN_sendMessages(void const *argument)
 {
+	bxCAN_CAN1_init();
+
 	// Infinite loop
 	for(;;)
 	{
@@ -44,6 +61,27 @@ void bxCAN_receiveMessages(void const *argument)
 //---------------------------------------------------------------------------
 // Initialization functions
 //---------------------------------------------------------------------------
+
+/**
+  * @brief  CAN1 configuration for bxCAN module.
+  * @param  None.
+  * @retval None.
+  */
+static void bxCAN_CAN1_init(void)
+{
+	canInit.CANx						= USE_CAN;
+	canInit.Timings.BaudratePrescaler 	= 5U;
+	canInit.Timings.TimeSegment1 		= CAN_TS1_TQ15;
+	canInit.Timings.TimeSegment2		= CAN_TS2_TQ2;
+	canInit.Timings.ResynchJumpWidth	= CAN_SJW_TQ1;
+	canInit.Mode						= CAN_MODE_NORMAL;
+	canInit.AutoBusOff					= ENABLE;
+	canInit.AutoWakeUp					= DISABLE;
+	canInit.AutoRetransmission			= ENABLE;
+	canInit.ReceiveFifoLocked			= DISABLE;
+	canInit.TransmitFifoPriority		= ENABLE;
+	CAN_init(&canInit);
+}
 
 /**
   * @brief  FreeRTOS initialization for bxCAN module.
