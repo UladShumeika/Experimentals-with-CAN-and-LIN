ef4f05c33488a5e733337efe96630ca350085ec9
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-05-11 10:31:03 +0200

    [ECU1_bxCAN.h/bxCAN.c] feat: add J1939_states enum

--- ECU1_429/Core/Inc/bxCAN.h
+++ ECU1_429/Core/Inc/bxCAN.h
@@ -10,6 +10,23 @@
 #include "main.h"
 
 //---------------------------------------------------------------------------
+// Structures and enumerations
+//---------------------------------------------------------------------------
+
+/**
+ * @brief SAE J1939 protocol states enumeration.
+ */
+typedef enum
+{
+	J1939_STATE_UNINIT,
+	J1939_STATE_NORMAL,
+	J1939_STATE_TP_RECEIVING_BROADCAST,
+	J1939_STATE_TP_SENDING_BROADCAST,
+	J1939_STATE_TP_RECEIVING_PEER_TO_PEER,
+	J1939_STATE_TP_SENDING_PEER_TO_PEER
+} J1939_states;
+
+//---------------------------------------------------------------------------
 // External function prototypes
 //---------------------------------------------------------------------------
 void bxCAN_freeRtosInit(void);
