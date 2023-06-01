20c8c78df81c822aa221422b954027bc6b1a4532
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-06-01 14:44:26 +0200

    [eeprom.c] feat: add eeprom_init_dma function

--- ECUs/Core/Src/eeprom.c
+++ ECUs/Core/Src/eeprom.c
@@ -16,6 +16,11 @@
 //---------------------------------------------------------------------------
 // Definitions
 //---------------------------------------------------------------------------
+#define PRJ_EEPROM_DMA_TX_PREEMPTION_PRIORITY					(5U)
+#define PRJ_EEPROM_DMA_TX_SUBPRIORITY							(0U)
+
+#define PRJ_EEPROM_DMA_RX_PREEMPTION_PRIORITY					(5U)
+#define PRJ_EEPROM_DMA_RX_SUBPRIORITY							(0U)
 
 //---------------------------------------------------------------------------
 // Macros
@@ -26,6 +31,9 @@
 //---------------------------------------------------------------------------
 static osThreadId m_eeprom_read_write_memory_handle = {0};
 
+static prj_dma_handler_t m_dma_tx = {0};
+static prj_dma_handler_t m_dma_rx = {0};
+
 //---------------------------------------------------------------------------
 // Variables
 //---------------------------------------------------------------------------
@@ -34,7 +42,9 @@ static osThreadId m_eeprom_read_write_memory_handle = {0};
 // Static functions declaration
 //---------------------------------------------------------------------------
 static void eeprom_read_write_memory_task(void const *p_argument);
+
 static uint32_t eeprom_init_gpio(void);
+static uint32_t eeprom_init_dma(void);
 
 //---------------------------------------------------------------------------
 // API
@@ -99,6 +109,56 @@ static uint32_t eeprom_init_gpio(void)
 	gpio_init.Speed			= GPIO_SPEED_VERY_HIGH;
 	gpio_init.Alternate 	= GPIO_AF4_I2C2;
 	status = GPIO_init(&gpio_init);
+
+	return status;
+}
+
+/*!
+ * @brief Initialize DMA peripherals for EEPROM memory.
+ *
+ * @return @ref PRJ_STATUS_OK if DMA initialization was successful.
+ * @return @ref PRJ_STATUS_ERROR if there are problems with the input parameters.
+ */
+static uint32_t eeprom_init_dma(void)
+{
+	uint32_t status = PRJ_STATUS_OK;
+
+	/* Enable DMA1 clock */
+	__RCC_DMA1_CLOCK_ENABLE();
+
+	/* DMA tx interrupt init */
+	MISC_NVIC_setPriority(DMA1_Stream7_IRQn, PRJ_EEPROM_DMA_TX_PREEMPTION_PRIORITY, PRJ_EEPROM_DMA_TX_SUBPRIORITY);
+	MISC_NVIC_enableIRQ(DMA1_Stream7_IRQn);
+
+	/* DMA tx init */
+	m_dma_tx.p_dma_stream						= DMA1_Stream7;
+	m_dma_tx.dma_init.channel					= PRJ_DMA_CHANNEL_7;
+	m_dma_tx.dma_init.direction					= PRJ_DMA_MEMORY_TO_PERIPH;
+	m_dma_tx.dma_init.periph_inc				= PRJ_DMA_PINC_DISABLE;
+	m_dma_tx.dma_init.mem_inc					= PRJ_DMA_MINC_ENABLE;
+	m_dma_tx.dma_init.periph_data_alignment		= PRJ_DMA_PERIPH_SIZE_BYTE;
+	m_dma_tx.dma_init.mem_data_alignment		= PRJ_DMA_MEMORY_SIZE_BYTE;
+	m_dma_tx.dma_init.mode						= PRJ_DMA_NORMAL_MODE;
+	m_dma_tx.dma_init.priority					= PRJ_DMA_PRIORITY_LOW;
+	m_dma_tx.dma_init.fifo_mode					= PRJ_DMA_FIFO_MODE_DISABLE;
+	status = prj_dma_init(&m_dma_tx);
+
+	/* DMA rx interrupt init */
+	MISC_NVIC_setPriority(DMA1_Stream2_IRQn, PRJ_EEPROM_DMA_RX_PREEMPTION_PRIORITY, PRJ_EEPROM_DMA_RX_SUBPRIORITY);
+	MISC_NVIC_enableIRQ(DMA1_Stream2_IRQn);
+
+	/* DMA rx init */
+	m_dma_rx.p_dma_stream						= DMA1_Stream2;
+	m_dma_rx.dma_init.channel					= PRJ_DMA_CHANNEL_7;
+	m_dma_rx.dma_init.direction					= PRJ_DMA_MEMORY_TO_PERIPH;
+	m_dma_rx.dma_init.periph_inc				= PRJ_DMA_PINC_ENABLE;
+	m_dma_rx.dma_init.mem_inc					= PRJ_DMA_MINC_DISABLE;
+	m_dma_rx.dma_init.periph_data_alignment		= PRJ_DMA_PERIPH_SIZE_BYTE;
+	m_dma_rx.dma_init.mem_data_alignment		= PRJ_DMA_MEMORY_SIZE_BYTE;
+	m_dma_rx.dma_init.mode						= PRJ_DMA_NORMAL_MODE;
+	m_dma_rx.dma_init.priority					= PRJ_DMA_PRIORITY_LOW;
+	m_dma_rx.dma_init.fifo_mode					= PRJ_DMA_FIFO_MODE_DISABLE;
+	status = prj_dma_init(&m_dma_rx);
 
 	return status;
 }
