820804840a9de3d001ab85f8de81476c8193edff
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-06-10 12:38:50 +0200

    [24lc256.h/24lc256.c] feat: add prj_eeprom_24lc256_connect_test function

--- ECUs/Drivers/24LC256/Inc/24lc256.h
+++ ECUs/Drivers/24LC256/Inc/24lc256.h
@@ -27,4 +27,20 @@
 // API
 //---------------------------------------------------------------------------
 
+/*!
+ * @brief Check device availability on the bus.
+ *
+ * This function is used to check the specified device availability several times.
+ * The number of checks is defined in @ref PRJ_24LC256_TRIALS.
+ *
+ * @param[in] p_i2c			A pointer to I2Cx peripheral.
+ * @param[in] dev_address	A target device address.
+ *
+ * @return @ref PRJ_STATUS_OK if the device is available.
+ * @return @ref PRJ_STATUS_ERROR if a null pointer to the peripheral is passed
+ * 		   or the device is not detected.
+ * @return @ref PRJ_STATUS_TIMEOUT if the timeout has passed.
+ */
+uint32_t prj_eeprom_24lc256_connect_test(I2C_TypeDef* p_i2c, uint16_t dev_address);
+
 #endif /* __24lc256_h */
--- ECUs/Drivers/24LC256/Src/24lc256.c
+++ ECUs/Drivers/24LC256/Src/24lc256.c
@@ -15,10 +15,12 @@
 //---------------------------------------------------------------------------
 #include "24lc256.h"
 #include "ush_stm32f4xx_conf.h"
+#include <stddef.h>
 
 //---------------------------------------------------------------------------
 // Definitions
 //---------------------------------------------------------------------------
+#define PRJ_24LC256_TRIALS									(5U)
 
 //---------------------------------------------------------------------------
 // Static functions declaration
@@ -28,6 +30,41 @@ static void eeprom_24lc256_write_protection(uint32_t state);
 //---------------------------------------------------------------------------
 // API
 //---------------------------------------------------------------------------
+
+/*!
+ * @brief Check device availability on the bus.
+ *
+ * This function is used to check the specified device availability several times.
+ * The number of checks is defined in @ref PRJ_24LC256_TRIALS.
+ *
+ * @param[in] p_i2c			A pointer to I2Cx peripheral.
+ * @param[in] dev_address	A target device address.
+ *
+ * @return @ref PRJ_STATUS_OK if the device is available.
+ * @return @ref PRJ_STATUS_ERROR if a null pointer to the peripheral is passed
+ * 		   or the device is not detected.
+ * @return @ref PRJ_STATUS_TIMEOUT if the timeout has passed.
+ */
+uint32_t prj_eeprom_24lc256_connect_test(I2C_TypeDef* p_i2c, uint16_t dev_address)
+{
+	uint32_t status = PRJ_STATUS_OK;
+
+	/* Check the pointer */
+	if(p_i2c == NULL)
+	{
+		status = PRJ_STATUS_ERROR;
+	}
+	else
+	{
+		/* DO NOTHING */
+	}
+
+	eeprom_24lc256_write_protection(PRJ_STATE_ENABLE);
+
+	status = prj_i2c_is_device_ready(p_i2c, dev_address, PRJ_24LC256_TRIALS);
+
+	return status;
+}
 
 //---------------------------------------------------------------------------
 // STATIC
