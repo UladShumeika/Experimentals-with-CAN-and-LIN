db8e9b600274759f48cc4c28a19fc6c2bab91359
Ulad Shumeika <wladshumeiko228@gmail.com>
2023-06-17 17:32:01 +0200

    [24lc256.h] feat: add PRJ_24LC256_MSG_SIZE define

--- ECUs/Drivers/24LC256/Inc/24lc256.h
+++ ECUs/Drivers/24LC256/Inc/24lc256.h
@@ -24,6 +24,8 @@
 #define PRJ_24LC256_I2C_USED						 (I2C2)
 #define PRJ_24LC256_I2C_CLOCK_SPEED					 (100000U)
 
+#define PRJ_24LC256_MSG_SIZE					 	 (130U)
+
 #define PRJ_24LC256_WP_ENABLED						 (0U)
 
 #if(PRJ_24LC256_WP_ENABLED == 1U)
